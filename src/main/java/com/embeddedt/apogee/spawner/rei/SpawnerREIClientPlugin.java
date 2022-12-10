f625666af95ce1584cecc41dc024dc3bc2299499
embeddedt <42941056+embeddedt@users.noreply.github.com>
2022-12-10 15:00:39 -0500

    Fix REI display

--- src/main/java/com/embeddedt/apogee/spawner/rei/SpawnerREIClientPlugin.java
+++ src/main/java/com/embeddedt/apogee/spawner/rei/SpawnerREIClientPlugin.java
@@ -13,6 +13,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpawnerREIClientPlugin implements REIClientPlugin {
@@ -25,10 +26,8 @@ public class SpawnerREIClientPlugin implements REIClientPlugin {
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
-        List<SpawnerModifier> recipes = Minecraft.getInstance().level.getRecipeManager().getAllRecipesFor(SpawnerModifier.TYPE);
+        List<SpawnerModifier> recipes = new ArrayList<>(Minecraft.getInstance().level.getRecipeManager().getAllRecipesFor(SpawnerModifier.TYPE));
         recipes.sort((r1, r2) -> r1.getOffhandInput() == Ingredient.EMPTY ? r2.getOffhandInput() == Ingredient.EMPTY ? 0 : -1 : 1);
-        recipes.forEach(recipe -> {
-            registry.add(new SpawnerDisplay(recipe));
-        });
+        recipes.forEach(recipe -> registry.add(new SpawnerDisplay(recipe)));
     }
 }
